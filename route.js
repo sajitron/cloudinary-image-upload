eb54926f56c182983b1b471ff0b3e1d2c9f11aeb
sajicode <sajioloye@gmail.com>
2019-01-22 03:00:25 +0100

    upload image successfully

--- route.js
+++ route.js
@@ -1,5 +1,7 @@
 const cloudinary = require('cloudinary');
 const keys = require('./config/keys');
+const multer = require('multer');
+const cloudinaryStorage = require('multer-storage-cloudinary');
 
 cloudinary.config({
 	cloud_name: keys.cloud_name,
@@ -7,8 +9,22 @@ cloudinary.config({
 	api_secret: keys.cloudinary_api_secret
 });
 
+const storage = cloudinaryStorage({
+	cloudinary,
+	folder: 'demo',
+	allowedFormats: [ 'jpg', 'jpeg', 'png' ],
+	transformation: [ { width: 500, height: 500, crop: 'limit' } ]
+});
+
+const parser = multer({ storage });
+
 module.exports = (app) => {
-	app.post('/api/upload', (req, res) => {
-		console.log(req.body);
+	app.post('/api/upload', parser.single('image'), (req, res) => {
+		console.log(req.file);
+		const image = {};
+		image.url = req.file.url;
+		image.id = req.file.public_id;
+
+		res.send(image);
 	});
 };
