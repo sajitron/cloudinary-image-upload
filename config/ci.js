21dd8d04eb1552285c1775f9c9ee34585eec8de3
sajicode <sajioloye@gmail.com>
2019-01-22 01:30:37 +0100

    set up basic app bolierplate

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+	googleClientID: '403862610013-m4t8l3drle9j7fdrb2f72iog44vjhadt.apps.googleusercontent.com',
+	googleClientSecret: '5m18r7r-JCJQBa0nGoph0Hdb',
+	mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+	cookieKey: 'saintsamiganja',
+	redisUrl: 'redis://127.0.0.1:6379'
+};
