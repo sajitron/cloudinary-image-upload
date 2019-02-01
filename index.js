219c5d4e51388e4439693db46393bef8b2cfa0da
sajicode <sajioloye@gmail.com>
2019-02-01 21:58:05 +0100

    add express fileUpload to index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(cors());
 
